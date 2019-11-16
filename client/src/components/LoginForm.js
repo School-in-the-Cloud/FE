4178eb046df593ede8cb1f381bd1ac3ba6cec67b
Robert Gordon <Komeuppance@gmail.com>
2019-11-15 18:37:42 -0800

    Login form complete

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import styled from 'styled-components';
+import {withFormik, Form, Field} from "formik";
+import * as Yup from 'yup';
+import axios from 'axios';
 
 const Container = styled.div`
     display: flex;
@@ -12,24 +15,103 @@ const Container = styled.div`
 const Login = styled.div`
     display: flex;
     flex-direction: column;
+    justify-content: center;
     background-color: white;
-    height: 400px;
-    width: 270px;
+    min-height: 350px;
+    width: 320px;
     border-radius: 10px;
     box-shadow: 0 9px 18px rgba(0, 0, 0, 0.3), 0 5px 12px rgba(0, 0, 0, 0.22);
-
+    button{
+        margin-top: 30px;
+        margin-bottom: 10px;
+        width: 150px;
+        height: 40px;
+        box-shadow: 2px 2px darkgray;
+        border-radius: 5px;
+        background-color: slategray;
+        color: white;
+        &:hover {
+        background-color: lightgray;
+    }
+}
 `
 
-function LoginForm() {
+const ErrorMsg = styled.div`
+    color: red;
+    font-size: 0.8rem;
+`
+
+const Dropdown = styled.div`
+    display: flex;
+    justify-content: center;
+
+    margin: 10px 0px;
+    font-size: 0.9rem;
+    text-align: center;
+    .select{
+        margin: 0 10px;
+        width: 120px;
+    }
+`
+
+const Checkbox = styled.div `
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 0.8rem;
+    span{
+        font-size: 0.7rem;
+        font-style: italic;
+        color: darkgray;
+    }
+`
+const LoginForm = ({values, errors, touched, status})=> {
+
+    function validateEmail(value) {
+        let error;
+        if (value === 'GordonRobert@gmail.com') {
+          error = 'Email already in use!';
+        }
+        return error;
+      }
+
     return (
         <>
         <Container>
             <Login>
-                Test
+                <Form>
+                    <p>Please enter the following information:</p>
+                    <Field type='email' name='email' placeholder='Email' validate={validateEmail} className='formfield' />
+                    {touched.email && errors.email && (<ErrorMsg>{errors.email}</ErrorMsg>)}
+                    <Field type='password' name='password' placeholder='Password' className='formfield' />
+                    {touched.password && errors.password && (<ErrorMsg>{errors.password}</ErrorMsg>)}
+                    <button type='submit'>Login!</button>
+                </Form>
             </Login>
         </Container>
         </>
     );
   }
 
-export default LoginForm
+const FormikLoginForm = withFormik({
+    mapPropsToValues({email, password}){
+        return {
+            email: email || '',
+            password: password || '',
+        };
+    },
+    validationSchema: Yup.object().shape({
+        email: Yup.string().email('Invalid email!').required('Email required!'),
+        password: Yup.string().min(6, 'Minimum 6 characters').required('Invalid password!'),
+    }),
+    handleSubmit(values, {setStatus}){
+    axios.post('', values)
+    .then (res =>{
+        setStatus(res.data);
+        console.log(res)
+    })
+    .catch (err => console.log(err.response))
+    }
+})(LoginForm);
+
+export default FormikLoginForm;
