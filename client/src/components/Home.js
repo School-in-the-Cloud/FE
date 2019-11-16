45958dc473a6101a4b510ec2dd2cbdb6c57d7167
Robert Gordon <Komeuppance@gmail.com>
2019-11-15 17:38:36 -0800

    Basic styling complete

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -6,6 +6,33 @@ const Head = styled.section`
   justify-content: center;
   align-items: center;
   flex-direction: column;
+  /* background-color: #b1bfd8;
+  background-image: linear-gradient(315deg, #b1bfd8 0%, #6782b4 74%); */
+  min-height: 100vh;
+  max-width: 100vw;
+  color: white;
+  text-shadow: 2px 2px 4px #000000;
+  h1{
+      margin:55px;
+      font-size:4rem;
+  }
+  div{
+      display: flex;
+      justify-content: space-between;
+      width: 30%;
+  }
+  button{
+      background-color: white;
+      color: black;
+      border-radius: 10px;
+      width: 155px;
+      height: 45px;
+      box-shadow: 0 9px 18px rgba(0, 0, 0, 0.3), 0 5px 12px rgba(0, 0, 0, 0.22);
+      margin: 30px;
+      &:hover{
+          background-color: lightblue;
+      }
+  }
 `
 
 function Home() {
@@ -15,9 +42,13 @@ function Home() {
         <h1>Welcome to School In The Cloud!</h1>
         <img
           className="main-img"
-          src="https://page-builder.ted.com/system/baubles/files/000/005/178/original/schoolcloud.png"
-          alt="Cloud"
+          src="https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/i_can_fly_7egl.svg"
+          alt="Cloud" width="450px"
         />
+        <div>
+            <button>Login</button>
+            <button>Sign Up</button>
+        </div>
     </Head>
     </>
   );
--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import styled from 'styled-components';
+
+const Container = styled.div`
+    display: flex;
+    width: 100vw;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+`
+
+const Login = styled.div`
+    display: flex;
+    flex-direction: column;
+    background-color: white;
+    height: 400px;
+    width: 270px;
+    border-radius: 10px;
+    box-shadow: 0 9px 18px rgba(0, 0, 0, 0.3), 0 5px 12px rgba(0, 0, 0, 0.22);
+
+`
 
 function LoginForm() {
     return (
         <>
+        <Container>
+            <Login>
+                Test
+            </Login>
+        </Container>
         </>
     );
   }
